31c9b102723f06dca651d97ca70ae614e9258ca1
Manuel Gomez <gomez.maman24@triana.salesianos.edu>
2024-10-02 14:30:28 +0200

    yo que se

--- Ufos/src/ufos_test.py
+++ Ufos/src/ufos_test.py
@@ -1,6 +1,33 @@
 from ufos import *
+import csv
+from datetime import datetime
+from math import sqrt
+from collections import namedtuple, Counter, defaultdict
+
+Avistamiento = namedtuple('Avistamiento', 'fechahora, ciudad, estado, forma, duracion, comentarios')
+fichero = "../data/ovnis.csv"
 
 # Test de la función lee_avistamientos
+
+
+def lee_avistamientos(fichero):
+    res = []
+    with open (fichero, encoding = "utf-8") as csvfile:
+        lector = csv.reader(csvfile)
+        for x in lector:
+            fecha_hora = x[0]
+            fechahora = datetime.strptime(fecha_hora, '%m/%d/%Y %H:%M')
+            ciudad = x[1]
+            estado = x[2]
+            forma = x[3]
+            duracion = int(x[4])
+            comentarios = x[5]
+            latitud = float(x[6])
+            longitud = float(x[7])
+            tupla = Avistamiento(fechahora,ciudad,estado,forma,duracion,comentarios,latitud,longitud)
+            res.append(tupla)
+        return res
+
 
 
 #Test de la función duracion_total
